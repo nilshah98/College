e99a6284a69dd760bbe7cc53a758567cbbbf1676
snidhiii <nidhi.sankhesara@somaiya.edu>
2019-01-14 18:01:23 +0530

    C file added

--- Pracs/SEM6/SPCC/hello.c
+++ Pracs/SEM6/SPCC/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main()
+void main()
 {
-   printf("Hello, World!");
-   return 0;
+	int a,b,c,sum;
+	printf("Enter values");
+	scanf("%d %d",&a,&b);
+	c = a+b;
+	printf("%d",sum);
+	return;
 }
