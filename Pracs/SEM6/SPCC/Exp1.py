2b4a8e0aee855036b9a622bf84f27b0de8990f3d
Freelancer-98 <neel12@somaiya.edu>
2019-01-20 22:58:42 +0530

    basic lexical analyser

--- Pracs/SEM6/SPCC/Exp1.py
+++ Pracs/SEM6/SPCC/Exp1.py
@@ -3,17 +3,36 @@ keywords = {
 "variable":[],
 "headerfile":[],
 "function":[],
-"condition":[],
-"loop":[]
+"operation":[]
 }
 
+dataTypes = ["int","char","float","bool","void"]
+operators = ["=","+","-","*","/"]
 #=================READ C FILE=========================== 
 f = open("./hello.c")
 program = f.read()
 f.close
 
 #=================SPLIT BY LINE=========================
-programLines = program.split("\n");
-print(programLines)
+programLines = program.split(";")
+for line in programLines:
+    newLines = line.split("\n")
+    for line in newLines:
+        if len(line)>0 and line[0] == "#":
+            keywords["headerfile"].append(line)
+        else:
+            tokens = line.split()
+            if len(tokens)>0 and tokens[0] in dataTypes:
+                if "(" and ")" in line:
+                    keywords["function"].append(line)
+                else:
+                    variables = tokens[1].split(",")
+                    for var in variables:
+                        keywords["variable"].append(var)
+            else:
+                if "(" and ")" in line:
+                    keywords["function"].append(line)
+                else:
+                    keywords["operation"].append(line)
 
-#=================SPLIT BY SPACE========================
+print(keywords)
