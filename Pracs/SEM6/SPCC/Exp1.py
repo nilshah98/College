6f3d25d5a7893bbb0e1bbc48d861e856dce9cb5c
snidhiii <nidhi.sankhesara@somaiya.edu>
2019-01-21 17:41:08 +0530

    Lex v1.1

--- Pracs/SEM6/SPCC/Exp1.py
+++ Pracs/SEM6/SPCC/Exp1.py
@@ -3,8 +3,10 @@ keywords = {
 "variable":[],
 "headerfile":[],
 "function":[],
-"operation":[]
+"operation":[],
 }
+
+special = []
 
 dataTypes = ["int","char","float","bool","void"]
 operators = ["=","+","-","*","/"]
@@ -15,24 +17,39 @@ f.close
 
 #=================SPLIT BY LINE=========================
 programLines = program.split(";")
+lineNum = 0
 for line in programLines:
     newLines = line.split("\n")
     for line in newLines:
+	if len(line)>0:
+		lineNum += 1
+	else:
+		continue
         if len(line)>0 and line[0] == "#":
-            keywords["headerfile"].append(line)
+            keywords["headerfile"].append([lineNum,line])
         else:
             tokens = line.split()
             if len(tokens)>0 and tokens[0] in dataTypes:
                 if "(" and ")" in line:
-                    keywords["function"].append(line)
+                    keywords["function"].append([lineNum,line])
                 else:
                     variables = tokens[1].split(",")
                     for var in variables:
-                        keywords["variable"].append(var)
+                        keywords["variable"].append([lineNum,var])
             else:
                 if "(" and ")" in line:
-                    keywords["function"].append(line)
+                    keywords["function"].append([lineNum,line])
                 else:
-                    keywords["operation"].append(line)
+                    keywords["operation"].append([lineNum,line])
 
-print(keywords)
+for key in keywords.keys():
+	print(key+"--->")
+	for token in keywords[key]:
+		if len(token[1])>0 and token[1] != "," and token[1] != "''" and token[1] !="{" and token[1] != "}":
+			print(token)
+		else:
+			special.append(token)
+	print("==============================================")
+print("special--->")
+for i in special:
+	print(i)
