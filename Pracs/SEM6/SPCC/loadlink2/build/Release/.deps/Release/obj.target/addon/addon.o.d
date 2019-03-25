d6db6e5e25b824371ef19ebcbe23742e0aeb2a31
hvshah97 <harshit.vs@somaiya.edu>
2019-03-25 17:41:08 +0530

    basic n-api examples added

---
+++ Pracs/SEM6/SPCC/loadlink2/build/Release/.deps/Release/obj.target/addon/addon.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/addon/addon.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/home/kjsce/.node-gyp/10.15.3/include/node -I/home/kjsce/.node-gyp/10.15.3/src -I/home/kjsce/.node-gyp/10.15.3/deps/openssl/config -I/home/kjsce/.node-gyp/10.15.3/deps/openssl/openssl/include -I/home/kjsce/.node-gyp/10.15.3/deps/uv/include -I/home/kjsce/.node-gyp/10.15.3/deps/zlib -I/home/kjsce/.node-gyp/10.15.3/deps/v8/include -I/home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/addon.o.d.raw   -c -o Release/obj.target/addon/addon.o ../addon.cc
+Release/obj.target/addon/addon.o: ../addon.cc \
+ /home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi.h \
+ /home/kjsce/.node-gyp/10.15.3/include/node/node_api.h \
+ /home/kjsce/.node-gyp/10.15.3/include/node/node_api_types.h \
+ /home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi-inl.h \
+ /home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi-inl.deprecated.h
+../addon.cc:
+/home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi.h:
+/home/kjsce/.node-gyp/10.15.3/include/node/node_api.h:
+/home/kjsce/.node-gyp/10.15.3/include/node/node_api_types.h:
+/home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi-inl.h:
+/home/kjsce/College/Pracs/SEM6/SPCC/loadlink2/node_modules/node-addon-api/napi-inl.deprecated.h:
